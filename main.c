5888d65ae069921ecba50d2f1ca64ae6c2db84ff
Thomas Flowers <thomas12flower@gmail.com>
2025-10-04 14:40:26 +0200

    fixed collect function and added to the main script

--- main.c
+++ main.c
@@ -7,9 +7,6 @@
 #define MAX 80
 #define ARRAY_SIZE 100
 
-
-
-
 typedef struct {
 
     int cell_number;
@@ -30,9 +27,9 @@ char* split(char string[MAX], char separator) {
 
     int index = 0;
     int set = 0;
-
+    
     static char second_half[MAX];
-
+    
     for(int i = 0; i < strlen(string); i++) {
        
       if(string[i] == separator) {
@@ -45,12 +42,12 @@ char* split(char string[MAX], char separator) {
 
 
     }
-
+    second_half[index] = '\0';
     return second_half;
 
 
-
 }
+
 
 
 int quit() {
@@ -78,8 +75,8 @@ int quit() {
 
 void collect() {
 
-    char user_input[MAX];
     Cell arr[ARRAY_SIZE];
+    char user_input[MAX];
     static int arr_index = 0; // want this to be a kind of global variable
 
 
@@ -106,9 +103,6 @@ void collect() {
             // printing the message
             printf("Network read from %s (added to position %d of the array)\n", filename, arr_index);
 
-          
-
-
             // dealing with the file
             FILE *rf = fopen(filename, "r"); 
 
@@ -118,164 +112,139 @@ void collect() {
             }
 
 
-
             // then we want to read from the given file
+            Cell current_cell;
             char current_line[MAX];
             int line_number = 1;
 
-            
+
             while(fgets(current_line, MAX, rf) != NULL) {
-                Cell current_cell = arr[arr_index];
 
                 // really didn't like this solution, if seek worked like python would've been easier
-                switch (line_number)
-                {
-                case 1:
-                    // case when it is just the cell name, need to extract the number
-                    char *cell_num = split(current_line, ' '); // should return the number
-                    int i_cell_num = atoi(cell_num);
+                switch (line_number) {
+                    case 1:
+                        // case when it is just the cell name, need to extract the number
+                        char *cell_num = split(current_line, ' '); // should return the number
+                        int i_cell_num = atoi(cell_num);
+                        current_cell.cell_number = i_cell_num;
+                        break;
 
-                   
-                    current_cell.cell_number = i_cell_num;
-                  
-
-
+                    case 2:
+                        char *address = split(current_line, ' ');
+                        address[strlen(address)-1] = '\0'; // get rid of the newline character
+                        strcpy(current_cell.address, address); 
+                        break;
                     
-                    break;
-                case 2:
-
-
-                    // for the address we again can look split on the whitespace
-                    char *address = split(current_line, ' ');
-                    strcpy(current_cell.address, address); // this may have a newline char at the end
-                    break;
-                
-                case 3:
-                    // ESSID - split on the " but also need to get rid of the last char
-                    current_line[strlen(current_line-1)] = "\0";
-                    printf("%s\n", current_line);
-
-
-                    char *essid = split(current_line, '"');
-
-
-                    strcpy(current_cell.ESSID, essid);
-
-                    break;
-                
-                case 4:
-                    // master, split on the :
-                    char *mode = split(current_line, ':');
-                    strcpy(current_cell.mode, mode);
-    
-
-                    break;
-                
-                case 5:
-                    // the channel
-                    char *channel = split(current_line, ':');
-                    int i_channel = atoi(channel);
-                    current_cell.channel = i_channel;
-
-
-                    break;
-                
-                case 6:
-                    // the encryption key
-                    char *encryption_key = split(current_line, ':');
-                    strcpy(current_cell.encryption_key, encryption_key);
-
-                   
-                    break;
-                
-                case 7:
-                    // quality 
-                    char *quality = split(current_line, '='); // will return a string like 50/90
-                    char first_num[2];
-                    char second_num[2];
-                    for (int i = 0; i < 2; i++) {
-                        first_num[i] = quality[i];
-                        second_num[i] = quality[i+3];
-                    }  
-
-                    int first = atoi(first_num);
-                    int second = atoi(second_num);
+                    case 3:
+                        char *essid = split(current_line, '"');
+                        essid[strlen(essid)-1] = '\0'; // get rid of the newline character
+                        strcpy(current_cell.ESSID, essid);
+                        break;
                     
-                    break;
-                
-                case 8:
-                    // frequency
-                    char *frequency = split(current_line, ':');
-                    char f[5];
-                    for (int i = 0; i < 5; i++){
-                        f[i] = frequency[i];
-
-                    }
-
-                    float freq = atof(f);
-                    current_cell.frequency = freq;
-
-                  
-                    break;
-                
-                case 9:
-                    // the signal level
-                    char *signal = split(current_line, '=');
-                    // then we only want the first 3 chars
-                    char sig[3];
-
-                    for(int i = 0; i < 3; i++){
-                        sig[i] = signal[i];
-                    }
-
-                    float s = atof(sig);
-
-
-
-
-
-
-                    break;
-                
-                default:
-                    break;
-                }
-               
-                line_number ++;
-                
-                if (line_number == 10) {
-                    // reset the index and also add the cell to the array
-                    line_number = 1;
-                    arr_index++;
-
+                    case 4:
+                        char *mode = split(current_line, ':');
+                        mode[strlen(mode)-1] = '\0'; // get rid of the newline character
+                        strcpy(current_cell.mode, mode);
+                        break;
                     
+                    case 5:
+                        char *channel = split(current_line, ':');
+                        int i_channel = atoi(channel);
+                        current_cell.channel = i_channel;
+                        break;
+                    
+                    case 6:
+                        // the encryption key
+                        char *encryption_key = split(current_line, ':');
+                        encryption_key[strlen(encryption_key)-1] = '\0'; // get rid of the newline character
+                        strcpy(current_cell.encryption_key, encryption_key);
+                        break;
+                    
+                    case 7:
+                        // quality 
+                        char *quality = split(current_line, '='); // will return a string like 50/90
+                        quality[strlen(quality)-1] = '\0'; // get rid of the newline character
+                    
+                        char first_num[3]; // need 3 for the null terminating char
+                        char second_num[3];
+                        for (int i = 0; i < 2; i++) {
+                            first_num[i] = quality[i];
+                            second_num[i] = quality[i+3];
+                        }  
+
+                        int first = atoi(first_num);
+                        int second = atoi(second_num);
+
+
+                        current_cell.quality_1 = first;
+                        current_cell.quality_2 = second;
+                        
+                        break;
+                    
+                    case 8:
+                        char *frequency = split(current_line, ':');
+                        char f[6];
+                        for (int i = 0; i < 5; i++){
+                            f[i] = frequency[i];
+
+                        }
+                        float freq = atof(f);
+                        current_cell.frequency = freq;
+                        break;
+                    
+                    case 9:
+                        // the signal level
+                        char *signal = split(current_line, '=');
+
+                        char sig[4];
+                        for(int i = 0; i < 3; i++){
+                            sig[i] = signal[i];
+                        }
+
+                        sig[3] = '\0';
+                        int s = atoi(sig);
+                        current_cell.signal_level = s;
+                    default:
+                        break;
                 }
 
-                // printf("%s\n", current_cell.address);
 
+                // if we have finished with the cell in the file
+                if (line_number == 9) {
 
-            }
+                    arr[arr_index] = current_cell; // add to the list
+                    current_cell = (Cell){0}; // reset the cell
+                    line_number = 1; // reset the cell line
+                    arr_index++; // increment the index of the arr of cells
 
-        printf("%d", arr[0].cell_number);
+                    
+                }  else {
+                    line_number ++;
+                }
 
-    
-            
-
-
-
-
-
-
-
-          
 
         }
 
+            fclose(rf);
+
+            // then now we need to print and format these cells
+            Cell cell = arr[0];
+            printf("id=[%d] mac=[%s] essid=[\"%s\"] mode=[3 (%s)] channel=[%d] key=[0 (%s)] q=[n1=%d n2=%d]\n", cell.cell_number, cell.address, cell.ESSID, cell.mode, cell.channel, cell.encryption_key, cell.quality_1, cell.quality_2);
 
 
+            // once this is done we need to ask if the user want to add another prompt
+            char access_point[MAX];
+            printf("Do you want to add another access point? [y:N]: ");
+            fgets(access_point, MAX, stdin);
 
+
+    
+        }
+          
     }
+
 }
+
 
 
 int main() {
@@ -337,15 +306,10 @@ int main() {
             }
 
            
-
-           
         }
     }
 
 
    
-
-
-
     return 0;
 }
